17f8665423cafa29694937d3415d8495ae6892bd
Vlada <vlada-ternova@mail.ru>
2023-06-16 14:48:03 +0300

    Домашняя работа

---
+++ задача 21/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите координаты 1 точки:");
+int ax = int.Parse(Console.ReadLine()!);
+int ay = int.Parse(Console.ReadLine()!);
+int az = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Введите координаты 2 точки:");
+int bx = int.Parse(Console.ReadLine()!);
+int by = int.Parse(Console.ReadLine()!);
+int bz = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Растояние равно {Math.Sqrt(Math.Pow(aх-bх,2)+Math.Pow(ay-by,2)+Math.Pow(az-bz,2)):f2}"); 
+
